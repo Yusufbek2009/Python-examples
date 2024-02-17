1a56f90237aadfbeb197b5ce6c290cb8a20cae43
Yu2009 <150792222+Yusufbek2009@users.noreply.github.com>
2024-02-17 18:50:09 +0500

    Create StringOperation.py

---
+++ StringOperation.py
@@ -0,0 +1,6 @@
+a = str(input("Enter random word here >> "))
+f = str(input("Enter random word piece here >> "))
+if f in a:
+  print("The word piece is from your word WOW")
+else:
+  print("The word piece isn't from your word")
